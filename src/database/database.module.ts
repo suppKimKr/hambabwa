8e405085812aaadc31c36efc08761476227d515c
suppKimKr <byjiwon@korea.ac.kr>
2022-08-14 20:35:05 +0900

    Project Base Setup

---
+++ src/database/database.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {ConfigService} from "@nestjs/config";
+
+@Module({
+  imports: [
+      TypeOrmModule.forRootAsync({
+        inject: [ConfigService],
+        useFactory: (configService: ConfigService) => ({
+          type: 'mysql',
+          host: configService.get('MYSQL_HOST'),
+          port: configService.get('MYSQL_PORT'),
+          username: configService.get('MYSQL_USER'),
+          password: configService.get('MYSQL_PASSWORD'),
+          database: configService.get('MYSQL_DB'),
+          logging: true,
+          entities: [
+            __dirname + '/../**/*.entity{.ts,.js}'
+          ],
+          timezone: 'Z',
+          synchronize: true,
+        }),
+      }),
+  ],
+})
+export class DatabaseModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,47 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {ConfigService} from "@nestjs/config";
+import * as cookieParser from 'cookie-parser';
+import {ValidationPipe} from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // .env 환경변수 관리
+  const configService = app.get(ConfigService);
+
+  // cookie-parser 사용
+  app.use(cookieParser());
+
+  // plainObject -> Class 자동 변환
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true
+  }));
+
+  // API 호출시 도메인 이후 가장 앞에는 /api 가 붙음
+  app.setGlobalPrefix('api');
+
+
+  /* API 문서 자동생성 Swagger 모듈 세팅 Start */
+  const configBuilder = new DocumentBuilder()
+      .setTitle('강남함바 ^_^?')
+      .setDescription('hambabwa API')
+      .setVersion('1.0')
+      .addTag('hamba')
+      .addBearerAuth(
+          { type: 'http', scheme: 'bearer', bearerFormat: 'JWT' },
+          'Authentication',
+      )
+      .build();
+
+  const document = SwaggerModule.createDocument(app, configBuilder);
+
+  SwaggerModule.setup('v1/api/docs', app, document, {
+    customCss: '.swagger-ui section.models { display: none;}'
+  });
+  /* Swagger End */
+
+  await app.listen(configService.get('PORT'));
 }
 bootstrap();
