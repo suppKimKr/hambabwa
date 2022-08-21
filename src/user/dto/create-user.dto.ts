5e01dc382b81b3e0a5ac036932401fa792c84454
suppKimKr <byjiwon@korea.ac.kr>
2022-08-21 21:56:09 +0900

    [add] create / update userDto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,21 @@
-export class CreateUserDto {}
+import {ApiProperty} from "@nestjs/swagger";
+
+export class CreateUserDto {
+    @ApiProperty({
+        description: "이메일",
+        example: "test@hambabwa.kr",
+    })
+    public email: string;
+
+    @ApiProperty({
+        description: "닉네임",
+        example: "김지원",
+    })
+    public nickname: string;
+
+    @ApiProperty({
+        description: "비밀번호",
+        example: "123",
+    })
+    public password: string;
+}
--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,4 +1,12 @@
-import { PartialType } from '@nestjs/swagger';
+import {ApiProperty, PickType} from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PickType(CreateUserDto, ['nickname'] as const) {
+    @ApiProperty({
+        type: 'string',
+        format: 'binary',
+        required: false,
+        description: "프로필 사진",
+    })
+    public imageUrl: string;
+}
