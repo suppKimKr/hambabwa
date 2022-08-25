2531b0e00d2d8f30ac0a6432b4468283d0090cdc
suppKimKr <byjiwon@korea.ac.kr>
2022-08-25 23:33:32 +0900

    [Update] RefreshToken, guards, logout feature

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,8 +1,9 @@
-import {HttpException, HttpStatus, Injectable} from '@nestjs/common';
+import {HttpException, HttpStatus, Injectable, UnauthorizedException} from '@nestjs/common';
 import {InjectRepository} from "@nestjs/typeorm";
 import {User} from "./entities/user.entity";
 import {Repository} from "typeorm";
 import {CreateUserDto} from "./dto/create-user.dto";
+import * as bcrypt from "bcrypt";
 
 @Injectable()
 export class UserService {
@@ -33,5 +34,29 @@ export class UserService {
     const currentHashedRefreshToken: string = await user.getCurrentRefreshToken(refreshToken);
     const userInstance: User = this.userRepository.create({ ...user, currentHashedRefreshToken });
     return await this.userRepository.save(userInstance);
+  }
+
+  async findUserById(id: number): Promise<User> {
+    const user = await this.userRepository.findOne({ id });
+    if (user) return user;
+    throw new HttpException(
+        "User with this id does not exists",
+        HttpStatus.NOT_FOUND,
+    )
+  }
+
+  async getUserIfRefreshTokenMatches(refreshToken: string, id: number): Promise<User> {
+    const user = await this.findUserById(id);
+    const isRefreshTokenMatching = await bcrypt.compare(refreshToken, user.currentHashedRefreshToken);
+
+    if (!isRefreshTokenMatching) {
+      throw new UnauthorizedException("Not found user for refresh token.");
+    }
+    return user;
+  }
+
+  async removeRefreshToken(id: number): Promise<User> {
+    const user = this.userRepository.create({id, currentHashedRefreshToken: null});
+    return await this.userRepository.save(user);
   }
 }
